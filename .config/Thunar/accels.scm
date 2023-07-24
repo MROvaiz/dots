24fd3021e596f7b079e86e6779e55aef76e68a4c
MROvaiz <rahilovaiz@gmail.com>
2023-07-24 20:40:52 +0530

    remaining changes

--- .config/Thunar/accels.scm
+++ .config/Thunar/accels.scm
@@ -82,6 +82,7 @@
 ; (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary>Page_Up")
 ; (gtk_accel_path "<Actions>/ThunarWindow/zoom-in-alt1" "<Primary>plus")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/select-all-files" "<Primary>a")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/dd8a308c9ab72e94ac22fa778763d688" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/open-in-new-window" "<Primary><Shift>o")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-configure-toolbar" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/view-location-selector-menu" "")
--- install.sh
+++ install.sh
@@ -1,1 +1,14 @@
-sudo pacman -S steam discord firefox hyprland pipewire wireplumber xdg-desktop-portal-hyprland grim slurp swappy kitty rofi mako thunar pavucontrol bluez bluez-utils blueman pacman-contrib ttf-jetbrains-mono-nerd noto-fonts-emoji lxappearance xfce4-settings 
+#install dependency
+sudo pacman -S ttf-jetbrains-mono-nerd noto-fonts-emoji pipewire wireplumber bluez bluez-utils pacman-contrib 
+
+#install hyprland
+sudo pacman -S hyprland xdg-desktop-portal-hyprland
+
+#install ultity applications
+sudo pacman -S grim slurp swappy kitty rofi mako thunar pavucontrol blueman lxappearance xfce4-settings hyprpaper pamixer
+
+#install applications
+sudo pacman -S steam discord firefox rclone fuse2 git code neovim
+
+
+#AUR install: sddm-git waybar-hyprland wlogout
